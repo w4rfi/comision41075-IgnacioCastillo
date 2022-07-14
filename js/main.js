7cc394bfe0ed72fa394b572dce291ea30cfb6cab
w4rfi <ignaciojc.95@hotmail.com>
2022-07-14 17:05:22 -0300

    Desafios clase3

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// Primer ejercicio 
+
+// let numeroSuma = parseInt(prompt('Ingrese un número para sumarle 10'));
+
+// for (let i = numeroSuma; i <= 1000; i = i + 10) {
+//     alert('Tu número es: ' + i );
+// }
+
+
+// Segundo ejercicio
+
+// let textoIngresado = prompt ('Ingrese su nombre para una cálida bienvenida')
+
+// while (textoIngresado != 'ESC') {
+//     alert('Bienvenido a Javascript: ' + textoIngresado)
+//     textoIngresado = prompt ('Ingrese otro nombre')
+// }
+
+
+// Tercer ejercicio 
+
+let numIngresado = parseInt(prompt ('Ingrese la cantidad de veces que desea ser saludado'));
+
+for(let i = 1; i <= numIngresado; i++){
+    console.log(i);
+    alert('Hola');
+}
